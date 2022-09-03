ab29c82a3edeabde21d10bf8518e5c8d413addf6
iamMAHAM <abdulkabore@gmail.com>
2022-09-03 17:04:55 +0000

    great

--- app.js
+++
@@ -1,6 +0,0 @@
-const pythagore = (object = {}, cote)=>{
-    const AC = Math.sqrt(Math.pow(object["AB"], 2) + Math.pow(object["BC"], 2))
-    console.log(`${cote} = ${AC}`)
-}
-
-pythagore({AB:8,BC:6}, "AC")
