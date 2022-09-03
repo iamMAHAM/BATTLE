80c7749aeabb000b469b4af62a85812f695146ac
iamMAHAM <abdulkabore@gmail.com>
2022-09-03 15:47:43 +0000

    function4

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const function4 = (string)=>{
     let voyelle = 0
     let consonne = 0
     let sup = ""
-    let total = 0
+    let totalc = 0
+    let totalv = 0
 
     for (let i = string.length - 1; i >= 0; i--){
         reversed += string[i]
@@ -14,16 +15,16 @@ const function4 = (string)=>{
     for (let i = 0; i < string.length; i++){
         let index = alphabet.indexOf(string[i])
         replaced += (index + 1)
-        "aeuioy".includes(string[i])
+        "aeuioy".includes(string[i]) ? totalv += (index + 1) :  totalc += (index + 1)
     }
     if (voyelle > consonne) sup = "voyelle"
     else if (voyelle < consonne) sup = "consonne"
-    else sup = egaux
+    else sup = "egaux"
 
 
-    return (`${reversed}, ${replaced}, ${consonne} et ${voyelle}, ${sup}`)
+    return (`${reversed}, ${totalc} et ${totalv}, ${sup}`)
 
 }
 
-let result = function4("bingerville")
+let result = function4("baba")
 console.log(result)
