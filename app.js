4c967a6eaaa6295470562c0a3c739c2f403c334c
iamMAHAM <abdulkabore@gmail.com>
2022-09-03 16:48:29 +0000

    push

--- app.js
+++ app.js
@@ -11,15 +11,12 @@ const arrayFunc = (array = [])=>{
     let diff = 0
     positifs.map(n => total_pos += n)
     negatifs.map(n => total_neg += n)
-
     diff = total_pos + total_neg
-    console.log(diff)
-
     if (diff > 0) res = "positif"
     else if (diff < 0) res = "négatif"
     else res = "égaux"
     return `${total_pos}, ${total_neg}, ${res}`
 }
 
-let rs = arrayFunc([7,-9,6,2,-8, "efez"])
+let rs = arrayFunc([7,-9,6,2,-8, 0])
 console.log(rs)
