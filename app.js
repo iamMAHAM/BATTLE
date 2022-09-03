60b61929ab79480302a8f628f05f8581cc3603f7
iamMAHAM <abdulkabore@gmail.com>
2022-09-03 16:08:32 +0000

    cool

--- app.js
+++ app.js
@@ -8,27 +8,28 @@ const primeNumber = (n)=>{
         i++
     }
     numbers.map(n=> {
-        let i  = 0
+        let i  = 2
         let flag = false
         while (i < n){
             if (n % i === 0){
                 flag = true
             }
+            i++
         }
         flag ? '' : primes.push(n)
     })
     let total = 0
     primes.map(n => total += n)
 
-    let j = 0
+    let j = 1
     while (j < total){
         (total % j) === 0 ? div.push(j) : ''
+        j++
     }
-
-    return `${primes}, "${div}"`
+    console.log(primes.join(", "))
+    console.log(div.join(" "))
 
 }
 
-let result = primeNumber(10)
-console.log(result)
+primeNumber(10)
 
