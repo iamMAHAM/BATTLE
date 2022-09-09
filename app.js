1e077977e71111017ca4e5c0b25b144c9ad3dc24
ABDUL MAHAM <98808330+iamMAHAM@users.noreply.github.com>
2022-09-09 10:44:22 +0000

    return insted of log

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const pythagore = (object = {}, cote)=>{
     const RES = Math.sqrt(Math.pow(Object.values(object)[0], 2) + Math.pow(Object.values(object)[1], 2))
-    console.log(`${cote} = ${RES}`)
+    return (`${cote} = ${RES}`)
 }
 
 pythagore({AB:8,BC:6}, "AC")
